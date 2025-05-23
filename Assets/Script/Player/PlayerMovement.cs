e853ae5aca2cf455c6ea8c90db47dc3ee2bd8c24
Nguyễn Thế Trung <162983251+SirKimCh@users.noreply.github.com>
2025-05-23 16:24:33 +0700

    Update

--- Assets/Script/Player/PlayerMovement.cs
+++ Assets/Script/Player/PlayerMovement.cs
@@ -9,7 +9,11 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float jumpForce = 10f;
     [SerializeField] private Animator playerAnimator;
     [SerializeField] private SpriteRenderer playerSpriteRenderer;
-
+    [SerializeField] private BoxCollider2D playerCollider;
+    [SerializeField] private LayerMask terrainLayer;
+    
+    private bool jumpCheck = false;
+    
     void Update()
     {
         Movement();
@@ -20,9 +24,18 @@ public class PlayerMovement : MonoBehaviour
     {
         float horizontal = Input.GetAxis("Horizontal");
         playerRigidbody.velocity = new Vector2(horizontal * moveSpeed, playerRigidbody.velocity.y);
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
         {
             playerRigidbody.velocity = new Vector2(playerRigidbody.velocity.x, jumpForce);
+        }
+        if (IsGrounded() == true)
+        {
+            jumpCheck = true;
+        }
+        if (IsGrounded() == false && jumpCheck == true && Input.GetKeyDown(KeyCode.Space))
+        {
+            playerRigidbody.velocity = new Vector2(playerRigidbody.velocity.x, jumpForce);
+            jumpCheck = false;
         }
     }
 
@@ -37,14 +50,31 @@ public class PlayerMovement : MonoBehaviour
             playerSpriteRenderer.flipX = false;
         }
 
-        if (playerRigidbody.velocity.x == 0 & playerRigidbody.velocity.y == 0)
+        if (Input.anyKey == true)
         {
-            playerAnimator.SetInteger("State", 0);
+            playerAnimator.SetBool("IsMove", true);
         }
-
-        if (playerRigidbody.velocity.x > 0)
+        else
+        {
+            playerAnimator.SetBool("IsMove", false);
+        }
+        if (playerRigidbody.velocity.y > .1f)
         {
             playerAnimator.SetInteger("State", 1);
         }
+        else if (playerRigidbody.velocity.y < -.1f)
+        {
+            playerAnimator.SetInteger("State", -1);
+        }
+        else
+        {
+            playerAnimator.SetInteger("State", 0);
+        }
+    }
+    
+    private bool IsGrounded()
+    {
+        return Physics2D.BoxCast(playerCollider.bounds.center, playerCollider.bounds.size, 0f, 
+            Vector2.down, 0.1f, terrainLayer);
     }
 }
